c9d2387782cdb5957b004d51597f812b57d8a37a
sukit <sukit12009@hotmail.com>
2020-12-15 01:13:26 +0700

    query data where date from database ok.

--- back/index.js
+++ back/index.js
@@ -10,7 +10,7 @@ const db = mysql.createConnection({
     user: "root",
     host: "localhost",
     password: "12345678",
-    database: "employeeSystem"
+    database: "room_cost"
 })
 
 app.get('/employees', (req, res) => {
@@ -65,6 +65,51 @@ app.delete('/employees/delete/:id', (req, res) => {
         }
     })
 })
+
+app.get('/roomFee', (req, res) => {
+    const day = req.query.day;
+    const dayBefore = new Date(day);
+    const dayBeforeString = new Date(dayBefore.setMonth(dayBefore.getMonth() - 1)).toISOString().split('T')[0];
+    // db.query("SELECT * FROM months WHERE date = ?", 
+    let afterDate;
+    let beforeDate;
+    db.query(
+        `SELECT months.date, 
+        months.unit_fire, months.unit_water, rooms.room_name, rooms.fee_room, rooms.fee_bin
+        FROM months
+        RIGHT JOIN rooms
+        ON months.room_id = rooms.room_id
+        WHERE months.date=?
+        ORDER BY months.room_id`,
+        [day],
+        (err, result) => {
+            if (err) {
+                console.log(err);
+            } else {
+                afterDate = result;
+                // res.send(result);
+                db.query(
+                    `SELECT months.date, 
+                    months.unit_fire, months.unit_water, rooms.room_name, rooms.fee_room, rooms.fee_bin
+                    FROM months
+                    RIGHT JOIN rooms
+                    ON months.room_id = rooms.room_id
+                    WHERE months.date=?
+                    ORDER BY months.room_id`,
+                    [dayBeforeString],
+                    (err, result) => {
+                        if (err) {
+                            console.log(err);
+                        } else {
+                            beforeDate = result;
+                            let a = [...afterDate, ...beforeDate]
+                            res.send(a);
+                        }
+                    });
+            }
+        });
+});
+
 
 app.listen('3001', () => {
     console.log('Server is running on port 3001')
--- front/src/page/roomFee/index.js
+++ front/src/page/roomFee/index.js
@@ -1,7 +1,26 @@
-function RoomFee () {
+import Axios from "axios";
+import { useState } from "react";
+
+function RoomFee() {
+    const [roomFee, setRoomFee] = useState([]);
+    const [date, setDate] = useState("");
+
+    const getroomFee = () => {
+        Axios.get(`http://localhost:3001/roomFee/?day=${date}`
+        ).then((response) => {
+            setRoomFee(response.data);
+        });
+    };
+
     return (
         <div>
             Room
+            <input type="date" 
+            onChange={(event) => {
+                setDate(event.target.value)
+            }} />
+            <button className="btn btn-success" onClick={getroomFee}> Show </button>
+            {/* {getroomFee()} */}
         </div>
     );
 }
